9c394eba5795db092ecad38c1ad50ab3b4545601
[1akk1] <[akashthakur784@gmail.com]>
2023-04-12 10:39:49 +0530

    elementbytagname

--- dom.js
+++ dom.js
@@ -1,7 +1,6 @@
 //var headerTitle = document.getElementById('header-title');
 //headerTitle.style.border = 'solid 3px #000';
-var items= document.getElementsByClassName('list-group-item')
-items[2].style.backgroundColor = 'green';
-for(i=0;i<items.length;i++){
-    items[i].style.fontWeight = 'bold';
+var li= document.getElementsByTagName('li');
+for(i=0;i<li.length;i++){
+    li[i].style.backgroundColor = 'grey';
 }
