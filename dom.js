07533a92a66b35587967057a51aee5e603017fe5
[1akk1] <[akashthakur784@gmail.com]>
2023-04-12 21:33:48 +0530

    corrected not fully task 7

--- dom.js
+++ dom.js
@@ -25,41 +25,40 @@ var itemList = document.querySelector('#items');
 
 
 
-// create element
-var newDiv = document.createElement('div');
+// // create element
+// var newDiv = document.createElement('div');
 
-//   add class
-newDiv.className = 'hello';
+// //   add class
+// newDiv.className = 'hello';
 
-// add id
-newDiv.id = 'hello1';
+// // add id
+// newDiv.id = 'hello1';
 
-// set attribute
-newDiv.setAttribute('title', 'Hello Div');
+// // set attribute
+// newDiv.setAttribute('title', 'Hello Div');
 
-// create text node
-var newDivText = document.createTextNode('Hello World');
-newDiv.style.fontSize= '30px';
+// // create text node
+// var newDivText = document.createTextNode('Hello World');
+// newDiv.style.fontSize= '30px';
 
-// add text to div
-newDiv.appendChild(newDivText);
+// // add text to div
+// newDiv.appendChild(newDivText);
 
-// append to DOM Header
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
+// // append to DOM Header
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
 
-container.insertBefore(newDiv,h1);
+// container.insertBefore(newDiv,h1);
 
 //  append top DOM Item1
 var newLi = document.createElement('li')
-newLi.className = 'list-group-item';
+newLi.className = 'hello';
 newLi.id = 'yo';
 var newLiText = document.createTextNode('hello world');
 newLi.appendChild(newLiText);
 
 
 var liLi = (document.getElementById('items'));
-liLi.appendChild(newLi)
-var newLiLi = (`${newLi.innerHTMl}  ${liLi.innerHTML}`);
-console.log(newLi.innerHTML)
-console.log(liLi.textContent)
+
+console.log(`${newLi.innerHTML} ${liLi.innerHTML}`);
+
