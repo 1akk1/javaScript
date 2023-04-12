220bdaa3b66290b2bc4d7263ef4e6e055b5efad4
[1akk1] <[akashthakur784@gmail.com]>
2023-04-12 10:12:56 +0530

    byclassname

--- dom.js
+++ dom.js
@@ -1,5 +1,7 @@
-var headerTitle = document.getElementById('header-title');
-headerTitle.style.border = 'solid 3px #000';
-var elementa = document.getElementsByClassName('title1');
-elementa.style.text = 'bold';
-elementa.style.font-color = 'green';
+//var headerTitle = document.getElementById('header-title');
+//headerTitle.style.border = 'solid 3px #000';
+var items= document.getElementsByClassName('list-group-item')
+items[2].style.backgroundColor = 'green';
+for(i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
