c8638ae982c8a04fa3d6cc1c5c7e3c37f913c3d3
[1akk1] <[akashthakur784@gmail.com]>
2023-04-12 12:40:05 +0530

    corrected querselectorall

--- dom.js
+++ dom.js
@@ -1,6 +1,8 @@
 
-var secondItem = document.querySelectorAll('li:nth-child(2)');
-secondItem.style.color = 'green';
+var secondItem = document.querySelectorAll('li');
+secondItem[1].style.color='green';
+console.log(secondItem)
+
 
 
 var odd = document.querySelectorAll('li:nth-child(odd)');
