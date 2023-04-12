a41079d91ed50b5f40a1191f4ee6927eb1fc58f1
[1akk1] <[akashthakur784@gmail.com]>
2023-04-12 21:24:07 +0530

    dom task7

--- dom.js
+++ dom.js
@@ -1,26 +1,26 @@
 var itemList = document.querySelector('#items');
-// parent
-console.log(itemList.parentElement);
-// lastelementchild
-console.log(itemList.lastElementChild);
-// firstelementgchild
-console.log(itemList.firstElementChild);
-// firstchild
-console.log(itemList.firstChild);
-// lastchild
-console.log(itemList.lastChild);
-// editlastelementchild
-itemList.lastElementChild.textContent='4 Item';
-// nextsibling
-console.log(itemList.nextSibling);
-// nextelementsibling
-console.log(itemList.nextElementSibling);
-// previoussibling
-console.log(itemList.previousSibling);
-// previouselementsibling
-console.log(itemList.previousElementSibling); 
-// editprevioussibling
-itemList.previousElementSibling.style.color = 'red';
+// // parent
+// console.log(itemList.parentElement);
+// // lastelementchild
+// console.log(itemList.lastElementChild);
+// // firstelementgchild
+// console.log(itemList.firstElementChild);
+// // firstchild
+// console.log(itemList.firstChild);
+// // lastchild
+// console.log(itemList.lastChild);
+// // editlastelementchild
+// itemList.lastElementChild.textContent='4 Item';
+// // nextsibling
+// console.log(itemList.nextSibling);
+// // nextelementsibling
+// console.log(itemList.nextElementSibling);
+// // previoussibling
+// console.log(itemList.previousSibling);
+// // previouselementsibling
+// console.log(itemList.previousElementSibling); 
+// // editprevioussibling
+// itemList.previousElementSibling.style.color = 'red';
 
 
 
@@ -38,10 +38,28 @@ newDiv.id = 'hello1';
 newDiv.setAttribute('title', 'Hello Div');
 
 // create text node
-var newDivText = document.createTextNode('hello world');
+var newDivText = document.createTextNode('Hello World');
+newDiv.style.fontSize= '30px';
 
 // add text to div
 newDiv.appendChild(newDivText);
-console.log(newDiv);
+
+// append to DOM Header
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+container.insertBefore(newDiv,h1);
+
+//  append top DOM Item1
+var newLi = document.createElement('li')
+newLi.className = 'list-group-item';
+newLi.id = 'yo';
+var newLiText = document.createTextNode('hello world');
+newLi.appendChild(newLiText);
 
 
+var liLi = (document.getElementById('items'));
+liLi.appendChild(newLi)
+var newLiLi = (`${newLi.innerHTMl}  ${liLi.innerHTML}`);
+console.log(newLi.innerHTML)
+console.log(liLi.textContent)
