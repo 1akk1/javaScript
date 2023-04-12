17aa9c21b62b03a1f6215d4ff68e9e6f0a9ea06a
[1akk1] <[akashthakur784@gmail.com]>
2023-04-12 19:54:32 +0530

    all DOM manipulation

--- dom.js
+++ dom.js
@@ -1,11 +1,47 @@
+var itemList = document.querySelector('#items');
+// parent
+console.log(itemList.parentElement);
+// lastelementchild
+console.log(itemList.lastElementChild);
+// firstelementgchild
+console.log(itemList.firstElementChild);
+// firstchild
+console.log(itemList.firstChild);
+// lastchild
+console.log(itemList.lastChild);
+// editlastelementchild
+itemList.lastElementChild.textContent='4 Item';
+// nextsibling
+console.log(itemList.nextSibling);
+// nextelementsibling
+console.log(itemList.nextElementSibling);
+// previoussibling
+console.log(itemList.previousSibling);
+// previouselementsibling
+console.log(itemList.previousElementSibling); 
+// editprevioussibling
+itemList.previousElementSibling.style.color = 'red';
 
-var secondItem = document.querySelectorAll('li');
-secondItem[1].style.color='green';
-console.log(secondItem)
 
 
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for(var i = 0;i < odd.length;i++){
-     odd[i].style.backgroundColor = 'green';
-}
+// create element
+var newDiv = document.createElement('div');
+
+//   add class
+newDiv.className = 'hello';
+
+// add id
+newDiv.id = 'hello1';
+
+// set attribute
+newDiv.setAttribute('title', 'Hello Div');
+
+// create text node
+var newDivText = document.createTextNode('hello world');
+
+// add text to div
+newDiv.appendChild(newDivText);
+console.log(newDiv);
+
+
