6abe31d4ea453894455a865b94e0f68bd52bd646
[1akk1] <[akashthakur784@gmail.com]>
2023-04-12 09:07:45 +0530

    new dom

---
+++ dom.js
@@ -0,0 +1,5 @@
+var headerTitle = document.getElementById('header-title');
+headerTitle.style.border = 'solid 3px #000';
+var elementa = document.getElementsByClassName('title1');
+elementa.style.text = 'bold';
+elementa.style.font-color = 'green';
