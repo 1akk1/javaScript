2e163ea6ffbb8d4cd5e59913c8bb6ea02f4ddcb3
[1akk1] <[akashthakur784@gmail.com]>
2023-04-12 11:15:18 +0530

    queryselector

--- dom.js
+++ dom.js
@@ -1,6 +1,7 @@
 //var headerTitle = document.getElementById('header-title');
 //headerTitle.style.border = 'solid 3px #000';
-var li= document.getElementsByTagName('li');
-for(i=0;i<li.length;i++){
-    li[i].style.backgroundColor = 'grey';
-}
+var secondItem= document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';  
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.color = 'white';
