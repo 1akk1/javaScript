30f7762624762fc2b547003307ae3447f0758783
[1akk1] <[akashthakur784@gmail.com]>
2023-04-13 11:17:05 +0530

    task 8

--- dom.js
+++ dom.js
@@ -1,58 +1,56 @@
-var itemList = document.querySelector('#items');
-// // parent
-// console.log(itemList.parentElement);
-// // lastelementchild
-// console.log(itemList.lastElementChild);
-// // firstelementgchild
-// console.log(itemList.firstElementChild);
-// // firstchild
-// console.log(itemList.firstChild);
-// // lastchild
-// console.log(itemList.lastChild);
-// // editlastelementchild
-// itemList.lastElementChild.textContent='4 Item';
-// // nextsibling
-// console.log(itemList.nextSibling);
-// // nextelementsibling
-// console.log(itemList.nextElementSibling);
-// // previoussibling
-// console.log(itemList.previousSibling);
-// // previouselementsibling
-// console.log(itemList.previousElementSibling); 
-// // editprevioussibling
-// itemList.previousElementSibling.style.color = 'red';
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+
+// submit event
+form.addEventListener('submit',addItem);
+// delete event
+itemList.addEventListener('click',removeItem); 
+
+// add item func
+function addItem(e){
+    e.preventDefault();
+    //  get input value
+    var newItem = document.getElementById('item').value;
+    // create new element
+    var li = document.createElement('li');
+    li.className = "list-group-item"
+    // add text node
+    li.appendChild(document.createTextNode(newItem));
 
 
+    // create del btn
+    var deleteBtn = document.createElement('button');
+    // add class to del btn
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    // append text node
+    deleteBtn.appendChild(document.createTextNode('X'));
+    // append btn to li
+    li.appendChild(deleteBtn);
+    // append li to list
+    itemList.appendChild(li);
+
+    // create edit btn
+    var editBtn = document.createElement('button');
+    //  add btn class
+    editBtn.className = 'btn btn-info btn-sm float-right edit';
+    // append text node
+    editBtn.appendChild(document.createTextNode('E'));
+    // append btn to li
+    li.appendChild(editBtn);
+    // append li to list
+    itemList.appendChild(li);
+
+}
 
 
-// create element
-var newDiv = document.createElement('div');
-
-//   add class
-newDiv.className = 'hello';
-
-// add id
-newDiv.id = 'hello1';
-
-// set attribute
-newDiv.setAttribute('title', 'Hello Div');
-
-// create text node
-var newDivText = document.createTextNode('Hello World');
-newDiv.style.fontSize= '30px';
-
-// add text to div
-newDiv.appendChild(newDivText);
-
-// append to DOM Header
-var container = document.querySelector('header .container');
-var h1 = document.querySelector('header h1');
-
-container.insertBefore(newDiv,h1);
-
-// append top DOM Item1
+//  remove item
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('sure?')){
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
 
 
-var item1 = document.querySelector('#items li:first-child');
-var hello = document.createTextNode('Hello World');
-item1.parentNode.insertBefore(hello, item1);
