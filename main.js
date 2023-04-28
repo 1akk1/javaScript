46a56a8227ca1465f1c11c70686ac75f4870dbe0
[1akk1] <[akashthakur784@gmail.com]>
2023-04-28 15:18:41 +0530

    task10

---
+++ main.js
@@ -0,0 +1,32 @@
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click' ,() =>  {
+//     btn.styles.color='green'
+
+// });
+// btn.addEventListener('mouseover' , () => {
+//     btn.style.color='blue';
+
+// });
+// btn.addEventListener('mouseout', () => {
+//     btn.style.color='red';
+// });      
+
+
+
+myform.addEventListener('submit' , onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var time = document.getElementById('time').value; 
+    
+    localStorage.setItem('name',name);
+    localStorage.setItem('email', email);
+    localStorage.setItem('phone', phone);
+    localStorage.setItem('time', time);
+
+    console.log(nameInput.value,emailInput.value,phoneInput.value);
+}
