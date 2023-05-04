b227af1546d6336385f49088bd49f6e7371fd1e5
[1akk1] <[akashthakur784@gmail.com]>
2023-05-04 20:14:47 +0530

    task 11

--- main.js
+++ main.js
@@ -18,13 +18,13 @@ myform.addEventListener('submit' , onSubmit);
 
 function onSubmit(e) {
     e.preventDefault();
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-    var time = document.getElementById('time').value; 
-    
-    localStorage.setItem('name',name);
-    localStorage.setItem('email', email);
-    localStorage.setItem('phone', phone);
-    localStorage.setItem('time', time);
+    let myObj = {
+        name : document.getElementById('name').value,
+        email : document.getElementById('email').value,
+        phone : document.getElementById('phone').value,
+        time : document.getElementById('time').value 
+    };
+    let myObj_serial = JSON.stringify(myObj);
+    localStorage.setItem("myObj", myObj_serial);
+    console.log(myObj_serial);
 }
