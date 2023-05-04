9bd50290c05d78b67b1dfdaf528fc54932253b9c
[1akk1] <[akashthakur784@gmail.com]>
2023-05-04 22:52:04 +0530

    task 12 commented

--- main.js
+++ main.js
@@ -18,13 +18,15 @@ myform.addEventListener('submit' , onSubmit);
 
 function onSubmit(e) {
     e.preventDefault();
-
+// creating object
     let myObj = {
         name : document.getElementById('name').value,
         email : document.getElementById('email').value,
         phone : document.getElementById('phone').value,
         time : document.getElementById('time').value 
     };
+// to make the object a string
     let myObj_serial = JSON.stringify(myObj);
+    // setting the key value to  name element and creating different element
     localStorage.setItem(document.getElementById('name').value , myObj_serial);
 }
