c00cef47977a3fc15b021c30082351ace74784b5
[1akk1] <[akashthakur784@gmail.com]>
2023-06-13 21:37:15 +0530

    Storing BAP data to axios

--- main.js
+++ main.js
@@ -14,34 +14,52 @@
 
 
 
+// myform.addEventListener('submit' , onSubmit);
+// deletes.addEventListener('click' , onClick);
+// edits.addEventListener('click', onEdit);
+
+// function onSubmit(e) {
+//     e.preventDefault();
+// // creating object
+//     let myObj = {
+//         name : document.getElementById('name').value,
+//         email : document.getElementById('email').value,
+//         phone : document.getElementById('phone').value,
+//         time : document.getElementById('time').value 
+//     };
+// // to make the object a string
+//     let myObj_serial = JSON.stringify(myObj);
+//     var objName = document.getElementById('email').value;
+//     // setting the key value to  name element and creating different element
+//     localStorage.setItem(objName, myObj_serial);
+// }
+// function onClick(e){
+//     e.preventDefault();
+    
+//     localStorage.removeItem(document.getElementById('email').value);
+//     document.getElementById("myform").reset();
+// }
+// function onEdit(e){
+//     e.preventDefault();
+//     localStorage.removeItem(document.getElementById('email').value);
+// }
+
 myform.addEventListener('submit' , onSubmit);
-deletes.addEventListener('click' , onClick);
-edits.addEventListener('click', onEdit);
 
 function onSubmit(e) {
     e.preventDefault();
-// creating object
-    let myObj = {
-        name : document.getElementById('name').value,
-        email : document.getElementById('email').value,
-        phone : document.getElementById('phone').value,
-        time : document.getElementById('time').value 
-    };
-// to make the object a string
-    let myObj_serial = JSON.stringify(myObj);
-    var objName = document.getElementById('email').value;
-    // setting the key value to  name element and creating different element
-    localStorage.setItem(objName, myObj_serial);
-}
-function onClick(e){
-    e.preventDefault();
-    
-    localStorage.removeItem(document.getElementById('email').value);
-    document.getElementById("myform").reset();
-}
-function onEdit(e){
-    e.preventDefault();
-    localStorage.removeItem(document.getElementById('email').value);
-    
-    
+    // var name = document.getElementById('name').value;
+    // var email = document.getElementById('email').value;
+    // var phone = document.getElementById('phone').value;
+    // var time = document.getElementById('time').value; 
+
+    axios
+        .post('https://crudcrud.com/api/e140e4f94e344e999514a05b05cc827c/savingData',{
+            name : document.getElementById('name').value,
+            email : document.getElementById('email').value,
+            phone : document.getElementById('phone').value,
+            time : document.getElementById('time').value,
+
+        })
+
 }
