fc8c12280d1bcb8feeb4c85e51ce75231233d78a
[1akk1] <[akashthakur784@gmail.com]>
2023-06-14 15:06:21 +0530

    Saving BAP data using axios

--- BapDataUsingAxios.js
+++ BapDataUsingAxios.js
@@ -1,4 +1,5 @@
 myform.addEventListener('submit' , onSubmit);
+deletes.addEventListener('click' , onClick);
 
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -7,7 +8,7 @@ window.addEventListener("DOMContentLoaded", () => {
         .then(response => {
             console.log(response)
             for(var i=0;i<responde.data.length;i++){
-                console.log(response.data[i]);
+                console.log(response.data[i]);  
             }
         })
         .catch(error => {
@@ -26,3 +27,20 @@ function onSubmit(e) {
 
         })
 }
+function onClick(e){
+    e.preventDefault();
+    const userId = e.target.dataset.id;
+    deleteUser(userId);
+    expenses.removeChild(newExpense);
+    document.getElementById("form").reset();
+}
+function deleteUser(userId){
+    axios  
+        .delete(`https://crudcrud.com/api/e140e4f94e344e999514a05b05cc827c/savingData/${userId}`)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        })
+    }
